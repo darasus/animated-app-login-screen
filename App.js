d5832a5a62de23596b3dc935b7de125d9d019bb6
idaraseliya <ilya@gen25.com>
2018-03-29 15:07:46 +0200

    initial files

---
+++ App.js
@@ -0,0 +1,31 @@
+/**
+ * Core
+ */
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { StackNavigator } from 'react-navigation';
+/**
+ * Components
+ */
+import LoginScreen from './src/components/LoginScreen';
+
+/* END OF IMPORTS */
+
+export default class App extends React.Component {
+  render() {
+    return <AppStackNavigator />;
+  }
+}
+
+const AppStackNavigator = StackNavigator({
+  LoginScreen: { screen: LoginScreen }
+});
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+});
---
+++ README.md
@@ -0,0 +1,11 @@
+# Animated Login Screen in React Native(Expo)
+
+An example of React Native animations.
+
+## Installation Instructions
+
+```js
+ $ git clone https://github.com/nathvarun/Uber-App-UI-Clone-React-Native.git
+ $ cd /Uber-App-UI-Clone-React-Native
+ $ npm install
+```
